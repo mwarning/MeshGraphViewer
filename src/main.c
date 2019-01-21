c0d3d13908af56fd01183ff8a996eedc73da2fde
Moritz Warning <moritzwarning@web.de>
2019-01-21 15:46:16 +0100

    bump version

--- src/main.c
+++ src/main.c
@@ -47,7 +47,7 @@ static const char *g_help_text =
 // Run state
 static int g_is_running;
 
-static const char *g_version = "1.0.0";
+static const char *g_version = "1.0.1";
 
 const char* g_graph = NULL;
 const char* g_call = NULL;
