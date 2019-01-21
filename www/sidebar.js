c7bc92ed146abd709caea1ab4c8958be3763f182
Moritz Warning <moritzwarning@web.de>
2019-01-21 20:05:09 +0100

    adjust indentation

--- www/sidebar.js
+++ www/sidebar.js
@@ -1,31 +1,32 @@
 function createSidebar() {
-  return function (/*el*/) {
-    var self = this;
+	return function () {
+		var self = this;
 
-    // Needed to avoid render blocking
-    var gridBreakpoints = {
-      lg: [992, 446],
-      xl: [1200, 560]
-    };
+		// Needed to avoid render blocking
+		var gridBreakpoints = {
+			lg: [992, 446],
+			xl: [1200, 560]
+		};
 
-    self.onClick = function onClick() {
-      var visibility = new CustomEvent('visibility');
-      self.button.dispatchEvent(visibility);
-      self.container.classList.toggle('hidden');
-    }
+		self.onClick = function onClick() {
+			console.log("sidebar.onClick");
+			var visibility = new CustomEvent('visibility');
+			self.button.dispatchEvent(visibility);
+			self.container.classList.toggle('hidden');
+		}
 
-    self.getWidth = function getWidth() {
-      if (gridBreakpoints.lg[0] > window.innerWidth || self.container.classList.contains('hidden')) {
-        return 0;
-      } else if (gridBreakpoints.xl[0] > window.innerWidth) {
-        return gridBreakpoints.lg[1];
-      }
-      return gridBreakpoints.xl[1];
-    };
+		self.getWidth = function getWidth() {
+			if (gridBreakpoints.lg[0] > window.innerWidth || self.container.classList.contains('hidden')) {
+				return 0;
+			} else if (gridBreakpoints.xl[0] > window.innerWidth) {
+				return gridBreakpoints.lg[1];
+			}
+			return gridBreakpoints.xl[1];
+		};
 
-    self.container = document.getElementsByClassName('sidebar')[0];
-    self.button = document.getElementsByClassName('sidebarhandle')[0];
+		self.container = document.getElementsByClassName('sidebar')[0];
+		self.button = document.getElementsByClassName('sidebarhandle')[0];
 
-    return self;
-  };
+		return self;
+	};
 }
